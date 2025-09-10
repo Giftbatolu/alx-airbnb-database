0637526412a8a59dae03501e03ef5e565521a87d
giftbatolu <sekinatoyelami@gmail.com>
2025-09-10 14:03:36 +0100

    FIle: write a subquery in join_queries.sqland subqueries.sql file for question in README.md file

---
+++ database-adv-script/joins_queries.sql
@@ -0,0 +1,20 @@
+USE airbnb_database;
+
+-- Write a query using an INNER JOIN to retrieve all bookings and the respective users who made those bookings.
+SELECT users.first_name, users.last_name, users.email, bookings.start_date, bookings.end_date, bookings.total_price, bookings.status, bookings.property_id
+FROM users INNER JOIN bookings 
+ON users.user_id = bookings.user_id;
+
+-- Write a query using a LEFT JOIN to retrieve all properties and their reviews, including properties that have no reviews.
+SELECT properties.name, properties.description, properties.location, properties.pricepernight, reviews.rating, reviews.comment
+FROM properties LEFT JOIN reviews
+ON reviews.property_id = properties.property_id;
+
+-- Write a query using a FULL OUTER JOIN to retrieve all users and all bookings, even if the user has no booking or a booking is not linked to a user.
+SELECT users.first_name, users.last_name, users.email, bookings.start_date, bookings.end_date, bookings.total_price, bookings.status, bookings.property_id
+FROM users LEFT JOIN bookings 
+ON users.user_id = bookings.user_id
+UNION
+SELECT users.first_name, users.last_name, users.email, bookings.start_date, bookings.end_date, bookings.total_price, bookings.status, bookings.property_id
+FROM users RIGHT JOIN bookings 
+ON users.user_id = bookings.user_id;
---
+++ database-adv-script/subqueries.sql
@@ -0,0 +1,19 @@
+USE airbnb_database; -- To select the intended database
+
+-- Write a query to find all properties where the average rating is greater than 4.0 using a subquery.
+SELECT properties.property_id, properties.host_id, properties.name, properties.description, properties.location, properties.pricepernight
+FROM properties
+WHERE property_id IN (
+    SELECT reviews.property_id
+    FROM reviews
+    WHERE rating > 4
+);
+
+-- Write a correlated subquery to find users who have made more than 3 bookings.
+SELECT user_id, first_name, last_name, email
+FROM users
+WHERE (
+    SELECT COUNT(user_id)
+    FROM bookings
+    WHERE bookings.user_id = users.user_id
+) > 3;
