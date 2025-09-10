738fc6898c960fd6896e95ea418cec353a0e1d08
giftbatolu <sekinatoyelami@gmail.com>
2025-09-10 14:20:56 +0100

    FIle: write a subquery in join_queries.sqland subqueries.sql file for question in README.md file

--- database-adv-script/subqueries.sql
+++ database-adv-script/subqueries.sql
@@ -1,14 +1,22 @@
 USE airbnb_database; -- To select the intended database
 
 -- Write a query to find all properties where the average rating is greater than 4.0 using a subquery.
--- SELECT properties.property_id, properties.host_id, properties.name, properties.description, properties.location, properties.pricepernight - to select specific columns
-SELECT *
+SELECT properties.property_id, properties.host_id, properties.name, properties.description, properties.location, properties.pricepernight
 FROM properties
-WHERE property_id IN (
-    SELECT reviews.property_id
+WHERE (
+    SELECT AVG(reviews.rating)
     FROM reviews
-    WHERE rating > 4
-);
+    WHERE reviews.property_id = properties.property_id
+) > 4.0;
+
+
+-- SELECT *
+-- FROM properties
+-- WHERE property_id IN (
+--     SELECT reviews.property_id
+--     FROM reviews
+--     WHERE rating > 4
+-- );
 
 -- Write a correlated subquery to find users who have made more than 3 bookings.
 SELECT user_id, first_name, last_name, email
